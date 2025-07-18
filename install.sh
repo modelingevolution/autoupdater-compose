3ccf52974c610b62df05325411769a75d4d92b90
Rafal Maciag <rafal.maciag@modelingevolution.com>
2025-07-18 13:34:48 +0200

    Release v1.0.59

--- install.sh
+++ install.sh
@@ -18,10 +18,10 @@
 set -e
 
 # Print script version for debugging
-echo "AutoUpdater Install Script v1.0.58 ($(date '+%Y-%m-%d %H:%M:%S'))"
+echo "AutoUpdater Install Script v1.0.59 ($(date '+%Y-%m-%d %H:%M:%S'))"
 
 # Configuration variables
-AUTOUPDATER_VERSION="1.0.58"  # Replaced from autoupdater.version file
+AUTOUPDATER_VERSION="1.0.59"  # Replaced from autoupdater.version file
 
 # Global variables
 JSON_OUTPUT=false
